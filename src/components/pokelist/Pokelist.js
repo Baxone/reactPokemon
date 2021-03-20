200870337207c7648c961e01e0a382df166f9add
Juanan <jantonio.perez@estudioclandestino.es>
2021-03-20 23:09:22 +0100

    componente para la lista de pokemons. He modificado el maxWidth para que se ajuste un poco mejor

--- src/components/pokelist/Pokelist.js
+++ src/components/pokelist/Pokelist.js
@@ -1,39 +1,33 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonCard from '../pokecard/PokemonCard';
 import './Pokelist.scss';
+import { getPokemons } from '../../services/pokemonServices';
 
-const pokelist = [
-    {
-        id: 1,
-        name: 'hh',
-        evolution: 'ss'
-    },
-    {
-        id: 2,
-        name: 'hh',
-        evolution: 'ss'
-    },
-    {
-        id: 3,
-        name: 'hh',
-        evolution: 'ss'
-    },
-    {
-        id: 4,
-        name: 'hh',
-        evolution: 'ss'
-    },
-];
+function Pokelist(props) {
+    const page = props.params.page;
+    const [loading, setLoading] = useState([false])
+    const [pokelist, setPokemons] = useState([])
 
+    useEffect(() => {
+        setLoading(true);
+        getPokemons(page).then(pokemons => {
+            setPokemons(pokemons);
+            setLoading(false);
+        });
+    }, [page])
 
-function Pokelist() {
+    if (loading) return <div className="loading"><img src="http://cdn.lowgif.com/full/c052dced5bb8e43a-.gif" alt="" /></div>
+
     return (
         <section className="grid">
             {
-                pokelist.map(pokemon => (
-                    <div className="col-12 col-t-6 col-t-4" key={pokemon.id}>
-                        <PokemonCard />
+                pokelist.map(({ name, url }) => (
+                    <div className="col-12 col-t-6 col-d-4" key={url}>
+                        <PokemonCard
+                            name={name}
+                            url={url}
+                        />
                     </div>
                 ))
             }
