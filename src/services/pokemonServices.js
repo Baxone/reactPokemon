aeb414a5e0bb343f4ef7965ccd287fe7a3499dcf
Juanan <jantonio.perez@estudioclandestino.es>
2021-03-28 15:37:57 +0200

    modificación del servicio y de la función que carga la vista de pokemon para recibir por parámetro el id del pokemon a cargar y adecuarlo a la peticion del api-rest. creación del metodo que me busca el pokemon por nombre y que llama a la api para que me lo devuelva.

--- src/services/pokemonServices.js
+++ src/services/pokemonServices.js
@@ -1,13 +1,19 @@
-const pokeApiURL = 'https://pokeapi.co/api/v2/pokemon'
+const pokeApiURL = 'http://localhost:3000/pokemons'
 
-export async function getPokemons(page) {
-    const response = await fetch(`${pokeApiURL}?offset=${page}&limit=9`);
-    const data = await response.json();
-    return data.results;
-}
-
-export async function getOnePokemon(route) {
-    const response = await fetch(route.url);
+export async function getPokemons() {
+    const response = await fetch(`${pokeApiURL}`);
     const data = await response.json();
     return data;
+}
+
+export async function getOnePokemon(page) {
+    const response = await fetch(`${pokeApiURL}/${page}`);
+    const pokemon = await response.json();
+    return pokemon;
+}
+
+export async function getPokemonsByName(name) {
+    const response = await fetch(`${pokeApiURL}/search/${name}`);
+    const pokemon = await response.json();
+    return pokemon;
 }
