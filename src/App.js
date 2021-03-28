3dacf48c483b0b0030130957a36404daa1411857
Juanan <jantonio.perez@estudioclandestino.es>
2021-03-28 15:39:57 +0200

    creación y gestion de rutas con WOUTER, libreria muy liviana que permite gestionar rutas en react de una forma muy parecida que react-router pero sin ninguna dependencia. Modificación de algún estilo para que quede decente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import Pokelist from './components/pokelist/Pokelist';
+import PagePoke from './components/pagepoke/PagePoke';
 import './App.scss';
 
-import { Link, Route } from "wouter";
+import { Route, useLocation } from "wouter";
 
 function App() {
+  const [location, setLocation] = useLocation();
   return (
     <div className="App">
+      <div className="quarterTriangle1"></div>
+      <div className="quarterTriangle2"></div>
       <div className="container">
         <header className="App-header">
-          <input type="text" placeholder="Filtrar pokemons por nombre" />
+          <input type="text" placeholder="Filtrar pokemons por nombre" onInput={event => {
+            if (event.target.value !== "") {
+              setLocation('/search/' + event.target.value)
+            } else {
+              setLocation('/')
+            }
+          }} />
         </header>
         <Route
           component={Pokelist}
           path="/" />
         <Route
+          component={PagePoke}
+          path="/pokemon/:page" />
+        <Route
           component={Pokelist}
-          path="/:page" />
+          path="/search/:name" />
+
       </div>
+      <div className="quarterCircle1"></div>
+      <div className="quarterCircle2"></div>
     </div>
+
   );
 }
+
 
 export default App;
